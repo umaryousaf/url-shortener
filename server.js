7437d1d32c9e90cdd3d4e203bc77c1d095c830fc
umaryousaf <umaryousaf2002@gmail.com>
2021-01-18 02:37:34 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express =  require("express");
+const mongoose =  require("mongoose");
+const ShortUrl = require('./models/shortUrl')
+const app = express();
+
+mongoose.connect('mongodb+srv://umar:umar123@url-shortener.djsk8.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', async (req,res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', { shortUrls: shortUrls })
+})
+
+app.post('/shortUrls', async (req,res) => {
+    // console.log(req.body)
+    await ShortUrl.create({ full: req.body.fullUrl })
+    
+    res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+    if (shortUrl == null) return res.sendStatus(404)
+  
+    shortUrl.clicks++
+    shortUrl.save()
+  
+    res.redirect(shortUrl.full)
+})
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
